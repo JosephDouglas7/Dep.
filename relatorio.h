7bcfc149803fe41558ad1735fc1aa3476be43b36
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-12-28 10:20:04 -0300

    Create relatorio.h

---
+++ relatorio.h
@@ -0,0 +1,14 @@
+//Feito com a ajuda do Chat bing
+void moduloRelatorio(void);
+char menuRelatorio(void);
+
+void relatorio_do_aluno(void);
+void republica_estudantil(void);
+
+char* relatorioDoAluno(void);
+char* republicaEstudantil(void);
+
+void relatorioDoAlunoComParametro(char*);
+void relatorioRepublicaEstudantil(char*);
+
+char* getRepublicaEstudantil(char*);
